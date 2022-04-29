ef3d1e0611394faf9c5f12ff36871732103efc99
sthifer <sthifer21@gmail.com>
2022-04-29 19:17:10 +0200

    iteracion1

---
+++ iteracion1.js
@@ -0,0 +1,26 @@
+/* Dado el siguiente javascript usa forof para recorrer el array de películas, 
+genera un nuevo array con las categorías de las películas e imprime por consola 
+el array de categorías. Ten en cuenta que las categorías no deberían repetirse. 
+Para filtrar las categorías puedes ayudarte de la función .includes() */
+
+
+const movies = [
+    {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
+    {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
+    {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
+    {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
+]
+
+let categorias = [];
+
+for (const movie of movies) {
+
+    movie.categories.forEach(element => {
+        if(!categorias.includes(element)){
+            categorias.push(element);
+        }
+    });
+
+}
+
+console.log(categorias );
